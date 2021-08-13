a02b19320faf881f91c5c481663fc9d1b474352f
448197683 <82860436+448197683@users.noreply.github.com>
2021-08-13 10:02:29 +0800

    finish

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const geo = navigator.geolocation.getCurrentPosition(success, error);
 function success(e) {
-	console.log(e);
-	lat = e.coords.latitude;
-	lon = e.coords.longitude;
-
-	const button = document.querySelector("button");
-	button.addEventListener("click", (e) => {
-		const span = document.createElement("span");
-		const wrap = document.querySelector(".wrap");
-		span.id = `removeSpan`;
-		wrap.insertBefore(span, button);
-		span.innerHTML = `lat${lat} lng${lon}`;
-
-		if (span === true) {
-			wrap.parentNode.removeChild(span);
-		}
-	});
+  console.log(e);
+  lat = e.coords.latitude;
+  lon = e.coords.longitude;
 }
 function error(error) {
-	console.log(error);
+  console.log(error);
 }
+
+
+
+const button = document.querySelector('button');
+button.addEventListener('click', (e) => {
+  const span = document.createElement('span');
+  const wrap = document.querySelector('.wrap');
+  span.id = `removeSpan`;
+  wrap.insertBefore(span, button);
+  span.innerHTML = `lat${lat} lng${lon}`;
+
+  if (span === true) {
+    wrap.parentNode.removeChild(span);
+  }
+});
